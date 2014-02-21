5def50e6a42d35399fefae868a696112f57fe724
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2014-02-21 10:28:14 -0500

    Fixed big in get_input_source method

--- pylearn2/models/model.py
+++ pylearn2/models/model.py
@@ -242,7 +242,7 @@ class Model(object):
         Returns a string, stating the source for the input. By default the
         input source (when is the only one) is called 'features'.
         """
-        if hasattr(self, input_source):
+        if hasattr(self, 'input_source'):
             return self.input_source
         else:
             return 'features'
