9d537b095ef86e4ad3e1f6027dfb951f3c430f1e
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2014-06-06 13:48:11 -0400

    New ZCA implementation

--- pylearn2/datasets/nbest.py
+++ pylearn2/datasets/nbest.py
@@ -4,14 +4,39 @@ import logging
 
 from clint.textui import progress
 import numpy as np
+from scipy import linalg
+from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.decomposition import PCA
+from sklearn.utils import array2d, as_float_array
 import theano
 
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
-from pylearn2.datasets.preprocessing import ZCA
 
 
 log = logging.getLogger(__name__)
+
+
+class ZCA(BaseEstimator, TransformerMixin):
+    def __init__(self, regularization=10**-5, copy=False):
+        self.regularization = regularization
+        self.copy = copy
+
+    def fit(self, X, y=None):
+        X = array2d(X)
+        X = as_float_array(X, copy=self.copy)
+        self.mean_ = np.mean(X, axis=0)
+        X -= self.mean_
+        sigma = np.dot(X.T, X) / X.shape[1]
+        U, S, V = linalg.svd(sigma)
+        tmp = np.dot(U, np.diag(1/np.sqrt(S+self.regularization)))
+        self.components_ = np.dot(tmp, U.T)
+        return self
+
+    def transform(self, X):
+        X = array2d(X)
+        X_transformed = X - self.mean_
+        X_transformed = np.dot(X_transformed, self.components_.T)
+        return X_transformed
 
 
 class NBest(DenseDesignMatrix):
@@ -79,9 +104,9 @@ class NBest(DenseDesignMatrix):
             self.X -= self.X.mean(axis=0)
             self.X /= self.X.std(axis=0)
         if zca:
-            zca = ZCA(filter_bias=0)
+            zca = ZCA(regularization=0)
             zca.fit(self.X)
-            self.X = zca.apply(self.X)
+            self.X = zca.transform(self.X)
         if pca:
             pca = PCA(n_components=pca, whiten=True)
             pca.fit(self.X)
